c0397bfaa3efbf54fcc43473895f842f02890ec9
Alexandra Hockett <87495152+AlexandraHockett@users.noreply.github.com>
2024-09-10 13:18:23 +0100

    Added basic JSX elements and layout for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <main>ola</main>;
+  return (
+    <main>
+      <div>
+        <h1 className="total-text">0</h1>
+        <div className="input-container">
+          <input type="text" placeholder="Income or expense" />
+          <input type="number" />
+          <select>
+            <option value="income">Income</option>
+            <option value="expense">Expense</option>
+          </select>
+          <button>+</button>
+        </div>
+        <div>
+          <div className="card">
+            <div className="card-info">
+              <h4>Salary</h4>
+              <p>July 27th, 2024</p>
+            </div>
+            <p className="amount-text success">+5000€</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
