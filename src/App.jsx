cccdb4cf1448032ee8d868c3ba41465693f00f0e
AlexandraHockett <alexandramandio@gmail.com>
2024-09-11 12:57:29 +0100

    Implemented input field with React state to handle user input

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,41 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [statementType, setStatementType] = useState("income");
+
+  const handleChangeInputMessage = (e) => {
+    setInputMessage(e.target.value);
+  };
+
+  const handleChangeInputValue = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <main>
       <div>
         <h1 className="total-text">0</h1>
         <div className="input-container">
-          <input type="text" placeholder="Income or expense" />
-          <input type="number" />
-          <select>
+          <input
+            type="text"
+            placeholder="Income or expense"
+            onChange={handleChangeInputMessage}
+            value={inputMessage}
+          />
+          <input
+            type="number"
+            placeholder=""
+            onChange={handleChangeInputValue}
+            value={inputValue}
+          />
+          <select
+            onChange={(e) => {
+              setStatementType(e.target.value);
+            }}
+            value={statementType}
+          >
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </select>
